7fef90780b0955db5f38b752e5e84d5be83e8a17
Randall Scharpf <randall.scharpf@gmail.com>
2025-01-27 17:38:08 -0800

    rewrap main application code to prevent it from running during unit testing

--- applications/flight_computer/code.py
+++ applications/flight_computer/code.py
@@ -8,23 +8,27 @@ from drivers.ads1118 import (
     ADS1118_SAMPLE_RATE,
 )
 
-adc = ADS1118(
-    microcontroller.pin.PA13,
-    microcontroller.pin.PA12,
-    microcontroller.pin.PA14,
-    microcontroller.pin.PB17,
-)
-asyncio.run(adc.take_sample(ADS1118_MUX_SELECT.TEMPERATURE))
 
-aout = analogio.AnalogOut(microcontroller.pin.PA05)
+def test_ads1118():
+    adc = ADS1118(
+        microcontroller.pin.PA13,
+        microcontroller.pin.PA12,
+        microcontroller.pin.PA14,
+        microcontroller.pin.PB17,
+    )
+    print("temp:", asyncio.run(adc.take_sample(ADS1118_MUX_SELECT.TEMPERATURE)))
+
+    aout = analogio.AnalogOut(microcontroller.pin.PA05)
+
+    def vtoi(x):
+        return int(x / 3.1 * 65535)
+
+    for i in range(101):
+        x = 0.031 * i
+        aout.value = vtoi(x)
+        y = asyncio.run(adc.take_sample(ADS1118_MUX_SELECT.CH1_SINGLE_END))
+        print(x, ",", y)
 
 
-def vtoi(x):
-    return int(x / 3.1 * 65535)
-
-
-for i in range(101):
-    x = 0.031 * i
-    aout.value = vtoi(x)
-    y = asyncio.run(adc.take_sample(ADS1118_MUX_SELECT.CH1_SINGLE_END))
-    print(x, ",", y)
+if __name__ == "__main__":
+    test_ads1118()

aa5169f1a8b5417f0d2713c395a3e98ef260470c
Randall Scharpf <randall.scharpf@gmail.com>
2025-01-29 16:55:00 -0800

    purge and move flight_computer and testing_board applications

---
+++ applications/cdh_breakout_board_fc_sim/drivers/camera.py
@@ -0,0 +1,6 @@
+"""
+Driver class for the ArduCam 5MP Mini SPI breakout board.
+"""
+
+# the most up-to-date camera code is in a Jupyter notebook and has never been
+# in the GitHub. It should be added here as a class with appropriate control methods.
